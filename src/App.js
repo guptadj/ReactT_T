431f2655b3b8a155877a1858399c10e03083eac1
guptadj <guptadj@rknec.edu>
2024-04-03 18:25:20 +0530

    14

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-
-import './App.css';
+// import './App.css';
+import Alert from './components/Alert';
+// import Abt from './components/Abt';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [modetype, setmodetype] = useState("Enable Dark Mode")
+  const [mode, setmode] = useState("light")
+  const [alert, setalert] = useState(null)
+
+  const Showalert =(message,type)=>{
+    setalert({
+        message: message,
+        type: type
+    })
+    setTimeout(() => {
+      setalert(null)
+    }, 1500);
+  }
+
+  const toogle = () => {
+    if(mode==='light'){
+      setmode("dark")
+      setmodetype("Enable light Mode")
+      document.body.style.backgroundColor= "grey"
+      Showalert("Dark mode is Enabled", "success")
+    }
+    else{
+      setmode("light")
+      setmodetype("Enable Dark Mode")
+      document.body.style.backgroundColor= "white"
+      Showalert("light mode is Enabled","success")
+    }
+  }
+
+
+
   return (
     <>
-        <Navbar tital="Divyanshu" abttxt="abt_txt" />
-        <Textform heading="Entre Text to Analyse"/>
+        <Navbar tital="Divyanshu" abttxt="abt_txt" modetype={modetype} mode={mode} toogle={toogle} />
+        <Alert alert={alert}/>
+        <Textform Showalert={Showalert} heading="Entre Text to Analyse" mode={mode} />
+        {/* <Abt/> */}
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -7,33 +7,41 @@ export default function Textform(props) {
         // console.log("On Upper btn Click"+ Text);
         let newText = Text.toUpperCase();
         settext(newText);
+        props.Showalert("Converted to upper case","success")
     }
 
     const handelwpclick= ()=>{
         // console.log("On Upper btn Click"+ Text);
         let newText = Text.toLowerCase();
         settext(newText);
+        props.Showalert("Converted to lower case","success")
     }
 
     const handelclick= (event)=>{
         // console.log("On Upper btn Click f")
         settext(event.target.value);
     }
+    const handelclearclick= (event)=>{
+        let newText = Text.toLowerCase();
+        settext("");
+        props.Showalert("text cleared","success")
+    }
 
   return (
     <>
-        <div className="container">
+        <div className={`container bg-${props.mode} text-${props.mode==="dark"?"light":"dark"}`}>
             <h1 className="my-3" >
                 {props.heading}
             </h1>
-            <div className="mb-3">
-            <textarea className="form-control" onChange={handelclick} id="mybox" value={Text} rows="8"> </textarea>
+            <div className={`mb-3 bg-${props.mode} navbar-${props.mode}`}>
+            <textarea className={`form-control bg-${props.mode} text-${props.mode==="dark"?"light":"dark"}`} onChange={handelclick} id="mybox" value={Text} rows="8"> </textarea>
         </div>
         <button className="btn btn-primary mx-2 " onClick={handelupclick} >Upper Case </button>
         <button className="btn btn-primary mx-2 " onClick={handelwpclick} >Lower Case </button>
+        <button className="btn btn-primary mx-2 " onClick={handelclearclick} >clear</button>
         </div>
-        <div className="container my-3">
-            <h1>Summary Of Text </h1>
+        <div className={`container my-3 bg-${props.mode} text-${props.mode==="dark"?"light":"dark"}`}>
+            <h2>Summary Of Text </h2>
             <p>Your Text Conatin {Text.split(" ").length} Words and {Text.length} char</p>
             <p>{0.08*Text.split(" ").length} min To read</p>
             <h2>Preview</h2>
